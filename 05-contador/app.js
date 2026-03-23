ae869ff0092df62d9760e8a2f1047d1e8e2a0c37
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-23 16:56:38 -0300

    feat: iniciar função para criar linhas

--- 05-contador/app.js
+++ 05-contador/app.js
@@ -42,7 +42,7 @@ function contarNumerosMultiplos(quantidade) {
     return listaNumerosMultiplosDeCinco
 }
 
-function contarPotencia(quantidade) {
+function contarQuadrado(quantidade) {
     let listaMultiplosDeDois = []
 
     for (let cont = 0; cont <= quantidade; cont++) {
@@ -52,8 +52,14 @@ function contarPotencia(quantidade) {
     return listaMultiplosDeDois
 }
 
+function criarLinha(numero, par, impar, multiploCinco, numeroPontecial) {
+    //Aqui ela vai receber 5 números e deve criar na tela a tabela com os 5 números
+    //Aqui sim começa o front
+
+}
+
 console.log(criarListaNumero(2))
 console.log(contarNumerosPares(10))
 console.log(contarNumerosImpares(10))
 console.log(contarNumerosMultiplos(10))
-console.table(contarPotencia(10))
+console.table(contarQuadrado(10))
