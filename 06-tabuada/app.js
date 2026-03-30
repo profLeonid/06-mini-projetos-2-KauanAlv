2ea2caf821d5c0973418b147eb68f8fc88f80a11
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-30 15:07:32 -0300

    feat: adicionar função de divisão

--- 06-tabuada/app.js
+++ 06-tabuada/app.js
@@ -30,6 +30,17 @@ function calcularMultiplicacao (numero) {
     return listaMult
 }
 
+function calcularDivisao (numero) {
+    let listaDiv = []
+
+    for (let cont = 1; cont <= 10; cont++){
+        listaDiv.push(Number((numero / cont).toFixed(2)))
+    }
+
+    return listaDiv
+}
+
 console.log(calcularSoma(5))
 console.log(calcularSubtracao(5))
 console.log(calcularMultiplicacao(5))
+console.log(calcularDivisao(140))
