9a55d8d00fedf29654e3ffd09d6cd60f56b4f335
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-30 14:56:13 -0300

    feat: criar função de soma

--- 06-tabuada/app.js
+++ 06-tabuada/app.js
@@ -1,2 +1,13 @@
 'use strict'
 
+function calcularSoma (numero) {
+    let listaSoma = []
+
+    for (let i = 1; i <= 10; i++) {
+        listaSoma.push(numero + i)
+    }
+
+    return listaSoma
+}
+
+console.log(calcularSoma(5))
