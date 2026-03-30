194c16d4c00cb57921743e04af7ae2bee62cf615
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2026-03-30 14:59:34 -0300

    feat: criar função de subtração

--- 06-tabuada/app.js
+++ 06-tabuada/app.js
@@ -3,11 +3,22 @@
 function calcularSoma (numero) {
     let listaSoma = []
 
-    for (let i = 1; i <= 10; i++) {
-        listaSoma.push(numero + i)
+    for (let cont = 1; cont <= 10; cont++) {
+        listaSoma.push(numero + cont)
     }
 
     return listaSoma
 }
 
+function calcularSubtracao (numero) {
+    let listaSub = []
+
+    for (let cont = 1; cont <= 10; cont++) {
+        listaSub.push(numero - cont)
+    }
+
+    return listaSub
+}
+
 console.log(calcularSoma(5))
+console.log(calcularSubtracao(5))
